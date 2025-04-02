89cee5c76c3f17e8970bc70d5f32f7f0175e207c
Frederik Løvstad <Loevstad14@gmail.com>
2025-04-02 11:10:59 +0200

    burger

---
+++ javascript/burgermenu.js
@@ -0,0 +1,26 @@
+const menuItems = ["loebforsindet", "praktisk", "rute", "sponsorer", "Omsind", "tilmeld"]
+
+const menuState = { isOpen: false };
+
+function toggleMenu() { 
+    const menu = document.querySelector(".menu");
+
+    if (window.innerWidth <= 768) {
+        menuState.isOpen = !menuState.isOpen;
+        menu.style.display = menuState.isOpen ? "block" : "none";
+    }
+}
+
+function generateMenu() {
+        const menu = document.querySelector(".menu");
+        menu.innerHTML ="";
+
+    for(let i = 0; i < menuItems.length; i++) { 
+        let menuItem = document.createElement("a");
+        menuItem.textcontent = menuItems[i];
+        menuItem.href = "#";
+        menu.appendChild(menuItem);
+    }
+}
+
+document.querySelector(".burger").addEventListener("click", toggleMenu);
